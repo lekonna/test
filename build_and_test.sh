2001f9bb59051185a71db9d3381aa822756d0d9b
Janne Kotka <lekonna@gmail.com>
2019-02-12 10:16:41 +0200

    tests: added a test script
    added a script to compile and run the tests on the code
    its utilized by the pre-commit hook to make sure commits
    are valid
    
    Signed-off-by: Janne Kotka <lekonna@gmail.com>

---
+++ build_and_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir _test
+cd _test
+cmake ../
+if [ $? -ne 0 ]; then
+    echo "CMAKE failed"
+    cd ..
+    rm -rf _test
+    exit 1
+fi
+echo "CMAKE passed"
+make 
+if [ $? -ne 0 ]; then
+    echo "BUILD failed"
+    cd ..
+    rm -rf _test
+    exit 1
+fi
+echo "BUILD passed"
+./bin/runTests
+
+if [ $? -ne 0 ]; then
+    echo "TEST failed"
+    cd ..
+    rm -rf _test
+    exit 1
+fi
+echo "TEST passed"
+
+cd ..
+rm -rf _test
+
+echo "Done!"
+
+exit 0
